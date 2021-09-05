9b3fc9822ff1e675855d28b72a990ba39fc61298
DawidSkoPython <dawid.skomorochow.python@gmail.com>
2021-09-05 23:27:22 +0200

    add categories

--- src/app/components/home/Home.tsx
+++ src/app/components/home/Home.tsx
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from "react";
 
-import { IonPage, IonHeader, IonToolbar, IonTitle } from "@ionic/react";
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonSlides, IonSlide } from "@ionic/react";
 import axios from "axios";
+
+const slideOpts = {
+  initialSlide: 1,
+  speed: 400,
+};
 
 const Home: React.FC = () => {
   // parameters
@@ -9,8 +14,9 @@ const Home: React.FC = () => {
   const [highlights, setHighlights] = useState([]);
   const [features, setFeatured] = useState([]);
   useEffect(() => {
-    axios.get("https://devdactic.fra1.digitaloceanspaces.com/foodui/1.json").then((response) => {
+    axios.get("https://devdactic.fra1.digitaloceanspaces.com/foodui/home.json").then((response) => {
       console.log(response.data);
+      setCategories(response.data.categories);
     });
   }, []);
   return (
@@ -20,6 +26,19 @@ const Home: React.FC = () => {
           <IonTitle>Blank</IonTitle>
         </IonToolbar>
       </IonHeader>
+      <IonContent>
+        <IonSlides pager={true} options={slideOpts}>
+          <IonSlide>
+            <h1>Slide 1</h1>
+          </IonSlide>
+          <IonSlide>
+            <h1>Slide 2</h1>
+          </IonSlide>
+          <IonSlide>
+            <h1>Slide 3</h1>
+          </IonSlide>
+        </IonSlides>
+      </IonContent>
     </IonPage>
   );
 };
